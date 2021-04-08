827199a310e1712b8d1239b8515605ed14153e13
matt <matt@chia.net>
2021-04-08 17:02:31 +0100

    Removed unnecessary announcement functionality from a bunch of place

--- chia/consensus/blockchain_check_conditions.py
+++ chia/consensus/blockchain_check_conditions.py
@@ -1,12 +1,11 @@
-from typing import Dict, List, Optional, Set
-
-from chia.types.announcement import Announcement
+from typing import Dict, List, Optional
 from chia.types.coin_record import CoinRecord
 from chia.types.condition_var_pair import ConditionVarPair
 from chia.util.clvm import int_from_bytes
 from chia.util.condition_tools import ConditionOpcode
 from chia.util.errors import Err
 from chia.util.ints import uint32, uint64
+from chia.types.blockchain_format.sized_bytes import bytes32
 
 
 def blockchain_assert_my_coin_id(condition: ConditionVarPair, unspent: CoinRecord) -> Optional[Err]:
@@ -80,7 +79,7 @@ def blockchain_assert_relative_time_exceeds(condition: ConditionVarPair, unspent
     return None
 
 
-def blockchain_assert_announcement(condition: ConditionVarPair, announcements: Set[bytes]) -> Optional[Err]:
+def blockchain_assert_announcement(condition: ConditionVarPair, announcements: List[bytes32]) -> Optional[Err]:
     """
     Check if an announcement is included in the list of announcements
     """
@@ -93,7 +92,8 @@ def blockchain_assert_announcement(condition: ConditionVarPair, announcements: S
 
 def blockchain_check_conditions_dict(
     unspent: CoinRecord,
-    announcements: List[Announcement],
+    coin_announcement_names: List[bytes32],
+    puzzle_announcement_names: List[bytes32],
     conditions_dict: Dict[ConditionOpcode, List[ConditionVarPair]],
     prev_transaction_block_height: uint32,
     timestamp: uint64,
@@ -101,15 +101,16 @@ def blockchain_check_conditions_dict(
     """
     Check all conditions against current state.
     """
-    announcement_names = set([a.name() for a in announcements])
     for con_list in conditions_dict.values():
         cvp: ConditionVarPair
         for cvp in con_list:
             error = None
             if cvp.opcode is ConditionOpcode.ASSERT_MY_COIN_ID:
                 error = blockchain_assert_my_coin_id(cvp, unspent)
-            elif cvp.opcode is ConditionOpcode.ASSERT_ANNOUNCEMENT:
-                error = blockchain_assert_announcement(cvp, announcement_names)
+            elif cvp.opcode is ConditionOpcode.ASSERT_COIN_ANNOUNCEMENT:
+                error = blockchain_assert_announcement(cvp, coin_announcement_names)
+            elif cvp.opcode is ConditionOpcode.ASSERT_PUZZLE_ANNOUNCEMENT:
+                error = blockchain_assert_announcement(cvp, puzzle_announcement_names)
             elif cvp.opcode is ConditionOpcode.ASSERT_HEIGHT_NOW_EXCEEDS:
                 error = blockchain_assert_block_index_exceeds(cvp, prev_transaction_block_height)
             elif cvp.opcode is ConditionOpcode.ASSERT_HEIGHT_AGE_EXCEEDS:
--- chia/util/condition_tools.py
+++ chia/util/condition_tools.py
@@ -108,7 +108,7 @@ def created_outputs_for_conditions_dict(
     return output_coins
 
 
-def created_announcements_for_conditions_dict(
+def coin_announcements_for_conditions_dict(
     conditions_dict: Dict[ConditionOpcode, List[ConditionVarPair]],
     input_coin: Coin,
 ) -> List[Announcement]:
@@ -117,6 +117,14 @@ def created_announcements_for_conditions_dict(
         message = cvp.vars[0]
         announcement = Announcement(input_coin.name(), message)
         output_announcements.append(announcement)
+    return output_announcements
+
+
+def puzzle_announcements_for_conditions_dict(
+    conditions_dict: Dict[ConditionOpcode, List[ConditionVarPair]],
+    input_coin: Coin,
+) -> List[Announcement]:
+    output_announcements = []
     for cvp in conditions_dict.get(ConditionOpcode.CREATE_ANNOUNCEMENT_WITH_PUZZLEHASH, []):
         message = cvp.vars[0]
         announcement = Announcement(input_coin.puzzle_hash, message)
@@ -124,7 +132,7 @@ def created_announcements_for_conditions_dict(
     return output_announcements
 
 
-def announcements_names_for_npc(npc_list) -> List[bytes32]:
+def coin_announcements_names_for_npc(npc_list) -> List[bytes32]:
     output_announcements = []
     for npc in npc_list:
         for condition, cvp_list in npc.conditions:
@@ -133,6 +141,12 @@ def announcements_names_for_npc(npc_list) -> List[bytes32]:
                     message = cvp.vars[0]
                     announcement = Announcement(npc.coin_name, message)
                     output_announcements.append(announcement.name())
+    return output_announcements
+
+
+def puzzle_announcements_names_for_npc(npc_list) -> List[bytes32]:
+    output_announcements = []
+    for npc in npc_list:
         for condition, cvp_list in npc.conditions:
             if condition == ConditionOpcode.CREATE_ANNOUNCEMENT_WITH_PUZZLEHASH:
                 for cvp in cvp_list:
@@ -142,11 +156,19 @@ def announcements_names_for_npc(npc_list) -> List[bytes32]:
     return output_announcements
 
 
-def created_announcement_names_for_conditions_dict(
+def coin_announcement_names_for_conditions_dict(
     conditions_dict: Dict[ConditionOpcode, List[ConditionVarPair]],
     input_coin: Coin,
 ) -> List[bytes32]:
-    output = [an.name() for an in created_announcements_for_conditions_dict(conditions_dict, input_coin)]
+    output = [an.name() for an in coin_announcements_for_conditions_dict(conditions_dict, input_coin)]
+    return output
+
+
+def puzzle_announcement_names_for_conditions_dict(
+    conditions_dict: Dict[ConditionOpcode, List[ConditionVarPair]],
+    input_coin: Coin,
+) -> List[bytes32]:
+    output = [an.name() for an in puzzle_announcements_for_conditions_dict(conditions_dict, input_coin)]
     return output
 
 
